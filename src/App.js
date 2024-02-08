0ec50f109a0d9c4ba76f419448d0e9e37b7e93e4
Dineshborse <dineshborse3791@gmail.com>
2024-02-08 17:18:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useCookies } from "react-cookie";
+import { CookiesProvider } from "react-cookie";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+const App = () => {
+  const [userTokenCookie, setuserTokenCookie, removeuserTokenCookie] = useCookies(["token"])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register setuserCookie={setuserTokenCookie} userCookie={userTokenCookie} />}></Route>
+            <Route path="/login" element={<Login setuserCookie={setuserTokenCookie} userCookie={userTokenCookie} />}></Route>
+            <Route path="/dashboard" element={<Dashboard setuserCookie={setuserTokenCookie} userCookie={userTokenCookie} />}></Route>
+</Routes>
+        </BrowserRouter>
+      </CookiesProvider>
+    </>
+  )
 }
-
-export default App;
+export default App
